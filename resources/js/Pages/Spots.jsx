550b25b6886a57b37b2bfe68bf140dcec994b429
matheusmurray <matheus.murray@wolk.com.br>
2023-09-16 12:44:09 -0300

    feat(config): implement react

---
+++ resources/js/Pages/Spots.jsx
@@ -0,0 +1,15 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Spots({}) {
+    return (
+        <>
+            <div className='p-5 bg-gray-800 text-white'>
+                <div className='bg-gray-600 rounded-lg border border-gray-500 bg-opacity-75'>
+                    <p>@minitrovaostg2</p>
+                    <p>Mini Cooper S</p>
+                    <p>3/10.000</p>
+                </div>
+            </div>
+        </>
+    );
+}
