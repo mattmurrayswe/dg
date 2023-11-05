5fcdcc3bed3e2f5ee67d37ef65cbb24528ff2b89
matheusmurray <matheus.murray@wolk.com.br>
2023-11-05 17:06:42 -0300

    feat: o club

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,6 +54,20 @@
                             </div>
 
                         </a>
+                        <a href="{{ route('deck') }}" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                            <div>
+                                <div class="h-20 w-20 flex items-center justify-center rounded-full">
+                                    <img src="{{URL::asset('/image/logos-font-fasten/club.png')}}" alt="profile Pic" height="160" width="160">
+                                </div>
+
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">dg. Club</h2>
+
+                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                    Em meio à cena automotiva de Curitiba, os visionários Renato Carneiro e Davi Muniz fundaram o Club para reunir proprietários e promover encontros de superesportivos.
+                                </p>
+                            </div>
+
+                        </a>
                         <a href="{{ route('organizer') }}" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-20 w-20 flex items-center justify-center rounded-full">
