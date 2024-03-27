3a077177c5bf22e3668e528e1c9eb7d06f153ffd
matheusmurraydev <matheusmurraydev@gmail.com>
2024-03-27 13:18:54 -0300

    feat: estilizacao dos navegadores

--- resources/js/Pages/Events/Home.jsx
+++ resources/js/Pages/Events/Home.jsx
@@ -12,14 +12,14 @@ export default function Home({ }) {
         <div>
             <CssBaseline />
             <div style={bgDotsLighter} className='bg-center bg-gray-900 text-white min-h-screen'>
-                <div className="">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                <div className="max-w-7xl mx-auto p-6 lg:p-8">
+                    <div className="relative grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <Link
                             href={route('organizer')}
                             className="scale-100 p-6 bg-gray-800/50 bg-gradient-to-bl from-gray-700/50 via-transparent ring-1 ring-inset ring-white/5 rounded-lg shadow-gray-500/20 shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                         >
                             <div>
-                                <h2 className="mt-6 text-xl font-semibold text-white">Área do Visitante</h2>
+                                <h2 className="mt-6 text-xl font-semibold text-white">Área do Organizador</h2>
                                 <p className="mt-4 text-gray-400 text-sm leading-relaxed">
                                     Organize seus eventos automotivos. Emissão de ingressos, gateway de pagamentos, estatísticas dos seus eventos e disparos de campanhas de marketing.
                                 </p> 
